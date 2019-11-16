b93e005f792efdf841717fbb1b5571c44223fe7e
rcolleen <creynolds@ucsb.edu>
2019-11-15 16:49:11 -0800

    initialize repository

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <libraries>
+
+int find_determinant(Eigen::MAtrix3d sym_op){
+//returns +-1 for the determinant using eigen library
+//throw error if not 1or -1
+        int det
+	return det
+}
+
+int  find_trace(Eigen::Martix3d mat){
+}
+
+Eigen::Matrix3d load_sym_op()
+{//user input or file is read to load sym_op
+	//returns eigen matrix of sym op
+}
+
+
+class Symmetry_Operation(Eigen::Matrix3d rot, std::vector<doubles> translation){
+	public:
+		Eigen::Matrix3d rotation;
+		std::vector<doubles>  translation;
+		std::string check_op_type(Symmetry_Operation sym_op)
+///		std::string check_op_type(Symmetry_Operation sym_op){
+			//goes through list of checks and returns string of sym op type
+			//
+				if (find_trace(sym_op.rotation)
+
+				
+			}
+};
+
+
+int main(argc, argv&)
+{ //WHAT is the actual input????
+	map<Eigen::Matrix3d, vector,doublesym_op;
+	std::string op_type = check_op_type(sym_op);
+	std::cout<<"This is a "<<op_type<<std::endl;
+
+	}
+
